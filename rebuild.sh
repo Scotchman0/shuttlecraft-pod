3054b0373bb0d028f8301b9cdcacd88b8d91c8ca
wpr7 <wpr7@duke.edu>
2022-12-31 15:32:06 -0500

    modified readme

--- rebuild.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#temporary rebuild script
-docker build -t shuttlecraft-pod:latest .
--- retry.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#temporary script to restart livetest for debugging
-docker rm livetest
-docker run -it --name livetest -p 3000:3000 shuttlecraft-pod:latest /bin/bash
